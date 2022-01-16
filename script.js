54e598a45428f04215bddf13354193916736048c
Axmadullo2000 <axmadullo2000@gmail.com>
2022-01-16 12:52:59 +0500

    Integrate Loop to app

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 let numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+while (numberOfNews == '') {
+    numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+}
 let personalNewsDB = {
     count: numberOfNews,
     news: {},
@@ -10,7 +13,29 @@ let personalNewsDB = {
 };
 
 
-let lastViewNew = prompt("Oxirgi ko'rgan yangiliklarizdan biri?");
-let gradeFilm = +prompt("Unga qancha baho bergan bo'lar edingiz?");
-personalNewsDB.news[lastViewNew] = gradeFilm;
+for (let i = 0; i < numberOfNews; i++) {
+    let lastViewNew = prompt(`${i + 1} - postni nomini kiriting: `);
+    while ((lastViewNew == null) || (lastViewNew == '') || (lastViewNew.length > 50)) {
+        lastViewNew = prompt(`${i + 1} - postni nomini kiriting: `);
+    }
+    let gradeFilm = +prompt("Unga qancha baho bergan bo'lar edingiz?");
+    while (gradeFilm == '') {
+        gradeFilm = +prompt("Unga qancha baho bergan bo'lar edingiz?");
+    }
+    personalNewsDB.news[lastViewNew] = gradeFilm;
+}
+let mess = '';
+if (personalNewsDB.count < 10) {
+    mess = "Juda oz sonli yangliklar o'qilibdi";
+}
+else if (30 > personalNewsDB.count > 10) {
+    mess = "Siz klassik tomoshabinsiz";
+}
+else if (personalNewsDB.count > 30) {
+    mess = "Siz yangiliklar ishqibozisiz";
+}
+else {
+    mess = "Xato yuz berdi";
+}
+console.log(mess);
 console.log(personalNewsDB);
