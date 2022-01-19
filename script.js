b310849073139cdaccb54e13a1b3a8dc287c14ac
Axmadullo2000 <axmadullo2000@gmail.com>
2022-01-19 14:44:32 +0500

    OOP Project

--- script.js
+++ script.js
@@ -1,71 +1,76 @@
 "use strict";
+
 let numberOfNews;
-function StartProject() {   
-    numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
-    while (numberOfNews == '') {
-        numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
-    }
-}
-StartProject();
 let personalNewsDB = {
-    count: numberOfNews,
+    count: 0,
     news: {},
     actors: {},
     genres: [],
+    StartProject: function () {
+        personalNewsDB.count = +prompt("Siz qancha yangilik ko'rdingiz?");
+        while (personalNewsDB.count == '') {
+            personalNewsDB.count = +prompt("Siz qancha yangilik ko'rdingiz?");
+        }
+    },
+    rememberNews: function () {
+        let lastViewNew, gradeFilm, arr = [];
+        for (let i = 0; i < personalNewsDB.count; i++) {
+            lastViewNew = prompt(`${i + 1} - postni nomini kiriting: `);
+            arr.push(lastViewNew);
+            while ((lastViewNew == null) || (lastViewNew == '') || (lastViewNew.length > 50)) {
+                lastViewNew = prompt(`${i + 1} - postni nomini kiriting: `);
+                arr.push(lastViewNew);
+            }
+            gradeFilm = +prompt("Unga qancha baho bergan bo'lar edingiz?");
+            while (gradeFilm == '') {
+                gradeFilm = +prompt("Unga qancha baho bergan bo'lar edingiz?");
+            }
+            personalNewsDB.news[lastViewNew] = gradeFilm;
+        }
+        console.log(arr);
+    },
+    toggleVisibleMyDB: function (hidden) {
+        if (hidden) {
+            personalNewsDB.private = false;
+        }
+        personalNewsDB.private = true;
+    },
+    writeGenres: function () {
+        let genresList, arr = [];
+        for (let i = 0; i < 3; i++) {
+            genresList = prompt(`Sizning ${i + 1} - sevimli janringiz: `);
+            arr.push(genresList);
+            while (genresList == '' || genresList == null) {
+                genresList = prompt(`Sizning ${i + 1} - sevimli janringiz: `);
+                arr.push(genresList);
+            }
+        }
+        return arr;
+    },
+    ShowPersonLevel: function (count) {
+        let mess;
+        if (count < 10) {
+            mess = "Juda oz sonli yangliklar o'qilibdi";
+        }
+        else if (30 > count > 10) {
+            mess = "Siz klassik tomoshabinsiz";
+        }
+        else if (count > 30) {
+            mess = "Siz yangiliklar ishqibozisiz";
+        }
+        else {
+            mess = "Xato yuz berdi";
+        }
+        console.log(mess);
+    },
     private: false
 };
 
-let lastViewNew, gradeFilm;
-function rememberNews() {
-    for (let i = 0; i < numberOfNews; i++) {
-        let lastViewNew = prompt(`${i + 1} - postni nomini kiriting: `);
-        while ((lastViewNew == null) || (lastViewNew == '') || (lastViewNew.length > 50)) {
-            lastViewNew = prompt(`${i + 1} - postni nomini kiriting: `);
-        }
-        let gradeFilm = +prompt("Unga qancha baho bergan bo'lar edingiz?");
-        while (gradeFilm == '') {
-            gradeFilm = +prompt("Unga qancha baho bergan bo'lar edingiz?");
-        }
-        personalNewsDB.news[lastViewNew] = gradeFilm;
-    }    
-}
-rememberNews();
-
-
-let mess = '';
-function ShowPersonLevel() {
-    if (personalNewsDB.count < 10) {
-        mess = "Juda oz sonli yangliklar o'qilibdi";
-    }
-    else if (30 > personalNewsDB.count > 10) {
-        mess = "Siz klassik tomoshabinsiz";
-    }
-    else if (personalNewsDB.count > 30) {
-        mess = "Siz yangiliklar ishqibozisiz";
-    }
-    else {
-        mess = "Xato yuz berdi";
-    }
-}
-
-ShowPersonLevel();
-
-        /* Show MyDB */
-function ShowMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalNewsDB);
-    }
-}
-ShowMyDB();
-console.log(mess);
-// console.log(personalNewsDB);
-let question;
-function WriteYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        question = prompt(`Sizning ${i + 1} - sevimli janringiz: `);
-        personalNewsDB.genres[i] = question;
-        console.log(personalNewsDB.genres[i]);
-    }
-}
-WriteYourGenres();
-console.log(personalNewsDB);
+personalNewsDB.StartProject();
+personalNewsDB.rememberNews();
+personalNewsDB.toggleVisibleMyDB(personalNewsDB.private);
+let array = personalNewsDB.writeGenres();
+array.forEach(function (item, index, arr) {
+    console.log(item);
+});
+personalNewsDB.ShowPersonLevel(personalNewsDB.count);
